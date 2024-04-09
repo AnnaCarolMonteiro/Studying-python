bd04458299fa52a6f953b94826262124f7824fcd
Anna Carolina <131694163+AnnaCarolMonteiro@users.noreply.github.com>
2024-04-09 13:40:34 -0300

    Add files via upload
    tests

---
+++ aula1.py
@@ -0,0 +1,19 @@
+print (12,34, sep ="-", end ='\n') # sep é separador , é oq vai separar esses dois valores 
+print (15,18, sep ='.')
+print (15,18, sep ='.')
+#\r - return  \n- line feed - quebra linha - CRLF
+#\n - LF 
+
+# pyhton é de tipagem dinamica e forte (ja sabe qual o tipo de info q vc passa p ele /mais pratico)
+#diferencia todos os caracteres , letras maiusculas e minusculas nao sao a mesma coisa
+#strings - textos dentro de aspas
+print(1234)
+#aspas simples
+print ('teclado')
+#aspas duplas
+print ("keyboard")
+#ESCAPE - \ faz aparecer aspas no run
+print("lUIZ \" OTAVIO")
+#r pode ser dubstituido desse jeito 
+print(r"Luiz \" Otávio \" ")
+print('otavio "Luiz"')
---
+++ aula2.py
@@ -0,0 +1,12 @@
+#tipos int e float 
+#int - inteiro
+#float - decimais e afins 
+
+print(12)
+print(-12)
+
+#float, sempre usamos o ponto . p separar o numero decimal
+print(1.1)
+print(10.11)
+#função type - 
+#
