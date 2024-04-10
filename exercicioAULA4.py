c17bce98ed91606988fe0c37796e60353b8eb2bf
Anna Carolina <131694163+AnnaCarolMonteiro@users.noreply.github.com>
2024-04-10 16:25:12 -0300

    Add files via upload

---
+++ exercicioAULA4.py
@@ -0,0 +1,14 @@
+nome ='Anna Carolina'
+sobrenome = 'MOnteiro de Carvalho'
+idade = 19
+ano_nascimento = 2024 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.68
+
+
+print ('Nome:', nome)
+print ('Sobrenome:',sobrenome)
+print ('Idade:',idade)
+print ('Ano nascimento:', ano_nascimento)
+print ('É maior de idade:',maior_de_idade)
+print ('Altura em metros:', altura_metros)
