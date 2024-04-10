e9ea2f09d2d749b6809a361467ca8054c8fa1752
Anna Carolina <131694163+AnnaCarolMonteiro@users.noreply.github.com>
2024-04-10 16:24:08 -0300

    Add files via upload

---
+++ aula5.py
@@ -0,0 +1,31 @@
+# operações aritméticas
+adicao = 10 + 10
+print('adicao', adicao)
+
+subtracao = 10 - 5
+print('subtracao', subtracao)
+
+multiplicacao = 10 * 10
+print('multiplicacao',  multiplicacao)
+
+divisao = 10 / 2.2
+print('divisao ', divisao) #TODONUMERO Q VEM DEPOIS DO PONTO NÃO APARECE 
+
+
+divisao_inteira = 10 // 2.2
+print('Divisão Inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print ('Exponenciação', exponenciacao)
+
+modulo = 55 % 2 # resto da divisao
+print('Modulo', modulo )
+
+print (10 % 8 == 0 )# se o resultado for 0 é porque 
+print (16 % 8 == 0 ) # verdadeiro
+print (10 % 2 == 0 )# verdadeiro 
+print (15 % 2 == 0 ) # falso 
+print (16 % 2 == 0 ) # falso
+
+# python consegue fazer conta com int e float (sempre o resultado é float)
+# só não faz conta com str e int/float
