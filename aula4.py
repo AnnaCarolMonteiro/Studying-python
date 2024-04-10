70ff217d4421eadfef0c95b43fa5fce34d5c3072
Anna Carolina <131694163+AnnaCarolMonteiro@users.noreply.github.com>
2024-04-10 16:24:57 -0300

    Add files via upload

---
+++ aula4.py
@@ -0,0 +1,16 @@
+#pep 8 - guai de estilos do python - site
+# variáveis salvam algo na memória do computador, a PEP8 permite o uso de letras minusculas estejam no inicio de variaveis
+# permite o uso de _ underline e números 
+#o sinal de = é operador de atribuição. eke atribui um valor a  um nome
+#nome_variavel = expressão
+nome_completo = 'Anna Carolina Monteiro de Carvalho'
+soma_dois_mais_dois = 2+2 #literal 4 
+int_um = bool('1') # cuidado pq parece inteiro mas é boolean
+print(nome_completo, soma_dois_mais_dois)
+print(int_um, type(int_um))
+
+nome = 'anna'
+idade = 18
+maior_de_idade = idade >-18
+print('nome:', nome, 'idade:', idade)
+print('é maior?', maior_de_idade)
