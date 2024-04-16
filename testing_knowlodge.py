64e6311e6426ae88c2403a7e05a15ff70751cd67
Anna Carolina <131694163+AnnaCarolMonteiro@users.noreply.github.com>
2024-04-16 16:17:10 -0300

    Add files via upload

---
+++ testing_knowlodge.py
@@ -0,0 +1,32 @@
+names = ('Anna', 'Sara', 'Chris' , 'Ademar')
+middle_names = ('Carolina', ' Emanuelle', 'William', 'Neto')
+last_name= ('Carvalho','de Jesus','Nya', 'Seabra')
+
+print(names + middle_names)
+ 
+name1=('Anna ')
+name2=('Sara ')
+name3=('Chris ')
+name4=('Ademar ')
+
+middle_name1=('Carolina')
+middle_name2=('Emanuelle')
+middle_name3=('William')
+middle_names4= ('Neto')
+
+last_name1=(' Monteiro de Carvalho')
+last_name2=(' da Silva de Jesus')
+last_name3=(' Kyomi Nya')
+last_name4=(' Seabra FOnseca')
+
+print(name1 + middle_name1 + last_name1)
+print(name4 + middle_names4 + last_name4)
+print(name3 + middle_name3 + last_name3)
+print(name2 + middle_name2 + last_name2)
+
+#contas
+conta_1= 2 ** 3
+conta_2= 6+6
+conta_3= 6-19
+conta_4= 5*5/2+7
+print(conta_1+ conta_2 * conta_3 + conta_4) 
