1f640a4346316749cce746e895ca955afad2ecc5
Anna Carolina <131694163+AnnaCarolMonteiro@users.noreply.github.com>
2024-04-16 13:25:30 -0300

    Add files via upload

---
+++ aula6.py
@@ -0,0 +1,8 @@
+concatenacao = 'Anna' + ' ' + 'Carolina'
+print (concatenacao) #unir os valores
+
+#a repetição deve ser feita com um inteiro e uma string 
+a_dez_vezes = 'A' * 10
+tres_vezes_anna = 3 * 'Anna'
+print(a_dez_vezes)
+print(tres_vezes_anna)
