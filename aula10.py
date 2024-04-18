acb93f0d443d722713ad8560385255da520e75dc
Anna Carolina <131694163+AnnaCarolMonteiro@users.noreply.github.com>
2024-04-18 15:55:23 -0300

    Add files via upload

---
+++ aula10.py
@@ -0,0 +1,13 @@
+#nome = input('Qual o seu nome? ')# function que solicita dados para o usuário
+#print(f'o seu nome é {nome}' )
+#idade = input('Qual a sua idade? ')
+#print(f' Sua idade é {idade}')
+
+numero_1= (input ('Digite um número: '))
+numero_2= (input('Digite outro número: '))#funciona porque soma int com int 
+
+int_numero_1= int(numero_1)
+int_numero_2= int(numero_2)
+
+print(f'A soma dos números é {numero_1 + numero_2}') # 15 + 22 = 1522 - concatenaçã
+
